5564233bac82ff80d9930493de4537b246436ad5
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-09-30 19:55:11 +0300

    Add tests and finish Homework_22

--- src/volunteering/models.py
+++ src/volunteering/models.py
@@ -2,14 +2,24 @@ from django.db import models
 # Create your models here.
 
 from django.db.models import ImageField
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class Category(models.Model):
+class Saver(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class Category(Saver):
     name = models.CharField(_("name"), max_length=256, )
 
 
-class Opportunity(models.Model):
+class Opportunity(Saver):
     title = models.CharField(_("title"), max_length=100)
     description = models.TextField(_("description"), )
     photo = ImageField(_("photo"),
@@ -20,22 +30,30 @@ class Opportunity(models.Model):
     city = models.CharField(_("city"), max_length=150, blank=True, null=True, default=None)
 
 
-class Need(models.Model):
+class Need(Saver):
     title = models.CharField(_("title"), max_length=100)
     description = models.TextField(_("description"), )
     price = models.DecimalField(_("price"), decimal_places=2, max_digits=20, null=True, blank=True)
     donation = models.URLField(_("donation"), null=True, blank=True)
     accounting = models.ForeignKey(to="volunteering.Accounting", related_name="needs",
-                                   on_delete=models.CASCADE)
+                                   on_delete=models.CASCADE, null=True, blank=True)
     photo = ImageField(_("photo"), upload_to="",  # TODO upload_to related to needs
                        blank=True,
                        null=True)
-    category = models.ManyToManyField(to="volunteering.Category", related_name="needs", )
+    category = models.ManyToManyField(to="volunteering.Category", related_name="needs", blank=True, null=True, )
     city = models.CharField(_("city"), max_length=150, blank=True, null=True, default=None)
 
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        self.time_created = timezone.now()
+        return super().save(*args, **kwargs)
 
-class Accounting(models.Model):
+    def time_waiting(self):
+        return timezone.now() - self.time_created
+
+
+class Accounting(Saver):
     photo = ImageField(_("photo"), upload_to="",  # TODO upload_to related to needs
                        blank=True,
                        null=True)
-    description = models.TextField(_("description"), )
+    description = models.TextField(_("description"), max_length=256, )
