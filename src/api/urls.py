e6554976182a8f1a9376eb6ff0b8c241bbf05f59
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-18 22:13:40 +0300

    Fix code style

--- src/api/urls.py
+++ src/api/urls.py
@@ -65,21 +65,25 @@ urlpatterns = [
     path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger_docs"),
     path("auth/", include("rest_framework.urls")),
     path("auth/", include("djoser.urls.jwt")),
+
     path("needs/", AllNeedsView.as_view(), name="all_needs"),
     path("needs/<int:pk>/", RetrieveNeedView.as_view(), name="need"),
     path("needs/create/", CreateNeedView.as_view(), name="create_need"),
     path("needs/update/<int:pk>/", UpdateNeedView.as_view(), name="update_need"),
     path("needs/delete/<int:pk>/", DeleteNeedView.as_view(), name="delete_need"),
+
     path("opportunities/", AllOpportunitiesView.as_view(), name="all_opportunities"),
     path("opportunities/<int:pk>/", RetrieveOpportunityView.as_view(), name="all_opportunities"),
     path("opportunities/create/", CreateOpportunityView.as_view(), name="create_opportunity"),
     path("opportunities/update/<int:pk>/", UpdateOpportunityView.as_view(), name="update_opportunity"),
     path("opportunities/delete/<int:pk>/", DeleteOpportunityView.as_view(), name="delete_opportunity"),
+
     path("categories/", AllCategoriesView.as_view(), name="all_categories"),
     path("categories/<int:pk>/", RetrieveCategoryView.as_view(), name="category"),
     path("categories/create/", CreateCategoryView.as_view(), name="create_category"),
     path("categories/update/<int:pk>/", UpdateCategoryView.as_view(), name="update_category"),
     path("categories/delete/<int:pk>/", DeleteCategoryView.as_view(), name="delete_category"),
+
     path("accounting/", AllAccountingView.as_view(), name="all_accounting"),
     path("accounting/<int:pk>/", RetrieveAccountingView.as_view(), name="accounting"),
     path("accounting/create/", CreateAccountingView.as_view(), name="create_accounting"),
