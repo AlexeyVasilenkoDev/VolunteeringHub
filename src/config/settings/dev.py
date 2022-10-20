6cf71263ef6a9ee036106a2bb63fe451e8e56384
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-20 13:55:27 +0300

    Add postgres locally

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -11,12 +11,20 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "default": {
+    "default_postgres": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
         "HOST": os.environ.get("POSTGRES_HOST"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "PORT": os.environ.get("POSTGRES_PORT"),
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Vasylenko",
+        "HOST": "localhost",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "PORT": "5432",
     },
 }
