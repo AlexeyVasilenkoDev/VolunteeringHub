9031040452a98ebc6d810a83df752ef06854ffe1
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-21 12:59:59 +0300

    Fix CI/CD v.2

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -6,25 +6,37 @@ DEBUG = True
 
 CURRENT_ENV = "DEV"
 
-DATABASES = {
-    "default_sqlite": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
-    },
-    # "default_for_local_machine": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": "Vasylenko",
-    #     "HOST": "localhost",
-    #     "USER": "postgres",
-    #     "PASSWORD": "admin",
-    #     "PORT": "5432",
-    # },
-}
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "github_actions",
+            "HOST": "127.0.0.1:8000",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "PORT": "5432:5432",
+        },
+    }
+else:
+    DATABASES = {
+        "default_sqlite": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+        # "default_for_local_machine": {
+        #     "ENGINE": "django.db.backends.postgresql",
+        #     "NAME": "Vasylenko",
+        #     "HOST": "localhost",
+        #     "USER": "postgres",
+        #     "PASSWORD": "admin",
+        #     "PORT": "5432",
+        # },
+    }
