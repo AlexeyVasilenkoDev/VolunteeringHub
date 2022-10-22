d6c85853c7fb268d9b9d20f8107737891110b5f7
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-22 13:52:02 +0300

    Fix tests please

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,10 +1,14 @@
 import os
 
+# from dotenv import load_dotenv
+
 from config.settings.base import *  # NOQA
 
 DEBUG = True
 
 CURRENT_ENV = "DEV"
+
+# load_dotenv()
 
 if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
@@ -19,24 +23,19 @@ if os.environ.get("GITHUB_WORKFLOW"):
     }
 else:
     DATABASES = {
-        "default_sqlite": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
+            "NAME": os.environ.get('POSTGRES_DB'),
             "HOST": os.environ.get("POSTGRES_HOST"),
             "USER": os.environ.get("POSTGRES_USER"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
             "PORT": os.environ.get("POSTGRES_PORT"),
+            # "TEST": {"NAME": "test"},
         },
-        # "default_for_local_machine": {
-        #     "ENGINE": "django.db.backends.postgresql",
-        #     "NAME": "Vasylenko",
-        #     "HOST": "localhost",
-        #     "USER": "postgres",
-        #     "PASSWORD": "admin",
-        #     "PORT": "5432",
-        # },
+        "default_sqlite": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
     }
+
+print(DATABASES)
