ea76161bac1afd1f08c55a259729c76f4b5ccef8
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-10-25 14:40:02 +0300

    Fix CI/CD

--- commands/start_server.sh
+++ commands/start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+python src/manage.py makemigrations
 python src/manage.py migrate
 python src/manage.py runserver 0:8000
