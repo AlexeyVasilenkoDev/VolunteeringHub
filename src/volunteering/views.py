6b991915dd763d3fba21952a5c1ae66e4d30c4bc
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-11-04 23:23:40 +0200

    Add celery, redis, redisadmin and flower

--- src/volunteering/views.py
+++ src/volunteering/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render  # NOQA
 
 # Create your views here.
@@ -5,6 +6,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 
 from volunteering.models import Need
+from volunteering.tasks import generate_category, generate_user, generate_opportunity, generate_accounting, \
+    generate_need
 
 
 class AllNeeds(TemplateView):
@@ -24,3 +27,30 @@ class CreateNeed(CreateView):
     model = Need
     success_url = reverse_lazy("core:index")
     fields = ["title", "description", "price", "donation", "photo", "category", "city"]
+
+
+def user_generator(request):
+    generate_user.delay()
+    return HttpResponse("User created!")
+
+
+def category_generator(request):
+    generate_category.delay()
+    return HttpResponse("Category created!")
+
+
+def accounting_generator(request):
+    generate_accounting.delay()
+    return HttpResponse("Accounting created!")
+
+
+def need_generator(request):
+    generate_need.delay()
+    return HttpResponse("Need created!")
+
+
+def opportunity_generator(request):
+    generate_opportunity.delay()
+    return HttpResponse("Opportunity created!")
+
+
